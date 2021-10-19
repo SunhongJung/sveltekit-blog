535b8c5e135486028915a55fb716df58e7406fa8
SunhongJung <sunhong.jung@gmail.com>
2021-10-19 16:43:04 +0900

    tutorial-13

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,26 +12,65 @@
 </script>
 
 <script>
+    import { paginate, LightPaginationNav, PaginationNav } from 'svelte-paginate'
+
     export let posts;
+    let searchTerm = ""
+    let searchedPost = posts
+
+    $: searchedPost = posts.filter((post) => {
+        return post.title.includes(searchTerm)
+    })
+
+    let currentPage = 1
+    let pageSize = 4
+    $: paginatedItems = paginate({ 'items': searchedPost, pageSize, currentPage })
+
 </script>
 
 <h1>Posts</h1>
 
+<input type="text" place="search" bind:value={searchTerm}>
+
 <div class="posts">
-    {#each posts as post (post.id)}
+    {#each paginatedItems as post (post.id)}
         <div class="post">
             <h2>{post.title.substring(0, 20)}</h2>
             <p>{post.body.substring(0,80)}</p>
             <p class="link"><a sveltekit:prefetch href="{`/blog/${post.id}`}">Read More</a></p>
         </div>
+    {:else}
+        <p>No found</p>
     {/each}
 </div>
+
+{#if searchedPost.length}
+    <LightPaginationNav
+    totalItems="{searchedPost.length}"
+    pageSize="{pageSize}"
+    currentPage="{currentPage}"
+    limit="{1}"
+    showStepOptions="{true}"
+    on:setPage="{(e) => currentPage = e.detail.page}"
+    />
+{/if}
+
+<!-- <PaginationNav
+  totalItems="{searchedPost.length}"
+  pageSize="{pageSize}"
+  currentPage="{currentPage}"
+  limit="{1}"
+  showStepOptions="{true}"
+  on:setPage="{(e) => currentPage = e.detail.page}"
+/> -->
+
 
 <style>
     .posts {
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-gap: 20px;
+        margin: 30px 0;
     }
 
     .post {
